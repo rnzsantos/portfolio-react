7a05a3a5c483f18fb4b574aa5f066853d336c50c
rnzsantos <118595156+rnzsantos@users.noreply.github.com>
2023-01-27 16:15:45 +0800

    Modified

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,11 @@
 export default function Project(props) {
-  console.log(props)
   return (
     <>
       <div className="col-auto">
         <div className="card bg p-1" style={{ width: "250px" }}>
-          <a href={props.url} target="_blank">
+          <a href={`..assets/images/${props.url}`} target="_blank">
             <img
-              src={`../../public/${props.img}`}
+              src={`../assets/images/${props.img}`}
               className="card-img-top rounded"
               alt=""
             />
